fd26729e095583518ee9238720e96b3ed36be09c
Leo <leusbst@googlemail.com>
2025-11-06 19:37:39 +0100

    changes added from drive

--- main.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-from transformers import DistilBertTokenizer
-
-raw_dataset = load_dataset("imdb")
-print (raw_dataset["test"])
-
-
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
